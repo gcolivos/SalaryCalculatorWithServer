7b5b3a1f38dafa6f71db4d1667a68440fc530f3a
Guillermo Olivos <gcolivos@gmail.com>
2017-11-28 08:12:52 -0600

    fixed comma in JSON

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ var express = require('express');
 
 // Create an instance of express by calling the function retured above - gives us an object
 var app = express();
-var port = 5000;
+var port = process.env.PORT || 5000;
 
 // express static file serving - public is the folder name
 app.use(express.static('server/public'));
